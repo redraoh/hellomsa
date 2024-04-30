c5ff14cf59967c23e35bdb858e7731546b4e1016
Jang <kinskicom@gmail.com>
2024-04-30 14:42:24 +0900

    product service commit

---
+++ product-service/api/models/product.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Text, Float
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    pno = Column(Integer, primary_key=True, autoincrement=True, index=True)
+    name = Column(String(100), nullable=False)
+    description = Column(Text, nullable=False)
+    price = Column(Float, nullable=False)
+
+
---
+++ product-service/api/routes/product.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from api.database import get_db
+import api.models.product as sqlm
+import api.schema.product as pym
+
+router = APIRouter()
+
+@router.get("/")
+async def index():
+    return {"message": "Hello World"}
+
+
+@router.get("/products", response_model=pym.ProductCreate)
+async def list_products(db:Session = Depends(get_db)):
+    products = db.query(sqlm.Product).all()
+    return [pym.Product.from_orm(p) for p in products]
+
+@router.post("/products", response_model=pym.Product)
+async def create_product(product: pym.ProductCreate, db:Session = Depends(get_db)):
+
+    product = sqlm.Product(**product.model_dump())
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return pym.ProductCreate.from_orm(product)
