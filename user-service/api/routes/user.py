a2392e76ac976393da425b568c4210f36fc504cd
Jang <kinskicom@gmail.com>
2024-05-01 16:49:42 +0900

    minor issues have been resolved

--- user-service/api/routes/user.py
+++ user-service/api/routes/user.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, APIRouter
 from sqlalchemy.orm import Session
 
 from api.database import get_db
